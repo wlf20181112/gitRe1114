2433909212d45af186484c454cf50014fb83b0e4
wlf20181112 <842503781@qq.com>
2018-11-14 11:31:51 +0800

    SourceTree提交idea创建的项目

---
+++ gitDemo/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String string="idea1125创建";
+        System.out.println(string);
+    }
+}
