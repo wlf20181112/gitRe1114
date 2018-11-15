a5b5c63ae31404ba845fdfa802d54061832dc259
wlf20181112 <842503781@qq.com>
2018-11-15 18:33:50 +0800

    idea删了很多1833

--- gitDemo/src/com/company/Main.java
+++ gitDemo/src/com/company/Main.java
@@ -5,10 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         String string="idea1125创建";
-		string="SourceTree1349添加";
-		string="idea1759添加";
-		string="st1831修改";
-		string="idea添加1813";
         System.out.println(string);
     }
 }
