55b9780a9f932107b921e67061ca04538e4367ab
wlf20181112 <842503781@qq.com>
2018-11-15 18:31:30 +0800

    st1831修改

--- gitDemo/src/com/company/Main.java
+++ gitDemo/src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
         String string="idea1125创建";
 		string="SourceTree1349添加";
 		string="idea1759添加";
-		string="st1811修改";
+		string="st1831修改";
 		string="idea添加1813";
         System.out.println(string);
     }
