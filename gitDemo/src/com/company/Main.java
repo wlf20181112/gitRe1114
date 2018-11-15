742d83eb4e2e21fb3ae0107bde42107021951771
wlf20181112 <842503781@qq.com>
2018-11-15 19:06:26 +0800

    idea修改1906

--- gitDemo/src/com/company/Main.java
+++ gitDemo/src/com/company/Main.java
@@ -5,7 +5,10 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         String string="idea1125创建";
+
 		string="st新加1838"
+        string="idea新加1841";
+        string="idea新加1856";
         System.out.println(string);
     }
 }
