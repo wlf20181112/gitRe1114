a4eac2c8072b7cf1ebf27da12863e7b13c51472b
wlf20181112 <842503781@qq.com>
2018-11-15 18:39:14 +0800

    st新加1838

--- gitDemo/src/com/company/Main.java
+++ gitDemo/src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         String string="idea1125创建";
+		string="st新加1838"
         System.out.println(string);
     }
 }
