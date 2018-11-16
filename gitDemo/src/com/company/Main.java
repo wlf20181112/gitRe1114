8ded9087ea5b8bb6384058ba96a8b566f7faf41f
wlf20181112 <842503781@qq.com>
2018-11-16 15:21:27 +0800

    idea主干删干净1521

--- gitDemo/src/com/company/Main.java
+++ gitDemo/src/com/company/Main.java
@@ -4,11 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-       
-		String s="st主干修改1420";
-       String ideas="idea主干新增1414";
-       String ideas="idea分支修改1504";
-       String ideas="idea分支修改1514";
+
+		String s="st主干1420";
         System.out.println(string);
     }
 }
