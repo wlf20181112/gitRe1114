875239ce8c58078272d718d49bdc5c64a892b816
wlf20181112 <842503781@qq.com>
2018-11-16 10:37:31 +0800

    idea中主干修改1036

--- gitDemo/src/com/company/Main.java
+++ gitDemo/src/com/company/Main.java
@@ -8,7 +8,8 @@ public class Main {
 
 		string="st新加1838"
         string="idea新加1841";
-        string="idea修改1021";
+        string="idea修改1036";
+        string="idea新增主干1036";
         System.out.println(string);
     }
 }
